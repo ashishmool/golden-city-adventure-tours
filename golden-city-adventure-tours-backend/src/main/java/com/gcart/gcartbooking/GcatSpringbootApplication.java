ad341f9bb217c545a5b0899b581e0daabfbe9e00
Ashish Mool <asis.mool@gmail.com>
2023-12-21 13:56:44 +0545

    Springboot Setup Complete
    -- Initial Setup

---
+++ golden-city-adventure-tours-backend/src/main/java/com/gcart/gcartbooking/GcatSpringbootApplication.java
@@ -0,0 +1,18 @@
+package com.gcart.gcartbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcatSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GcatSpringbootApplication.class, args);
+    }
+
+
+}
+
+
+
+
+
---
+++ golden-city-adventure-tours-backend/src/test/java/com/gcart/gcartbooking/GcartBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcart.gcartbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class gcartBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
